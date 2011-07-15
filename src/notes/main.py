53d33acedfd3a0e1d6873809280d1f407ecbf5dc
Lugghawk <acidictadpole@gmail.com>
2011-07-15 15:27:46 -0400

    Initial commit

---
+++ src/notes/main.py
@@ -0,0 +1,14 @@
+'''
+Created on 2011-07-15
+
+@author: dl250074
+'''
+
+if __name__ == '__main__':
+    pass
+
+
+import mypygit
+
+
+repo = mypygit.Repo()
---
+++ src/notes/mypygit.py
@@ -0,0 +1,56 @@
+'''
+Created on 2011-07-15
+
+@author: dl250074
+'''
+import subprocess
+from os import path, mkdir
+
+class Repo(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.makeRepo()
+        
+    def makeRepo(self):
+        '''
+        Simply checking for a .git directory in the current directory
+        '''
+        homedir = path.expanduser("~")
+        repodir = path.normcase(homedir+"/sessionnotes/")
+        
+        if not path.lexists( repodir + "/.git")  :
+            print "Making git repo " + self.gitInit ( repodir ) 
+        else:
+            print "Git repo in" + repodir + " exists"
+
+
+    def gitInit (self, repodir):
+        '''
+        Init the repo.
+        
+        This likely won't be used at all.
+        '''
+        if not path.lexists ( repodir ):
+            mkdir (repodir)
+        cmd = 'git init'
+        pipe = subprocess.Popen(cmd, shell=True, cwd=repodir)
+        
+        pipe.wait()
+        
+        return repodir
+    
+    def gitClone (self,repodir,remoterepo):
+        '''
+        Clone a remote repo.
+        
+        This should be the default behaviour. I'm not sure where the remote repo will be stored by default, but this should be configurable
+        '''
+        
+        
