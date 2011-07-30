3bc73565a00c9b1537df9f3a5df09344587894d6
Lugghawk <acidictadpole@gmail.com>
2011-07-30 15:09:31 -0400

    Refactored a bunch of the methods used in mypygit

--- src/notes/mypygit.py
+++ src/notes/mypygit.py
@@ -19,69 +19,98 @@ class Repo(object):
         '''
         print "Repo class created at " + repopath or "<no path> given"
         try:
-            self.gitLocation = self.findGit()
+            Repo.gitLocation = self.findGit()
         except IOError:
             print "Git Not Found"
             os.sys.exit(2)
-            
-        self.makeRepo(repopath, remoteRepo)
         
-    def makeRepo(self, repopath, remoterepo=None):
+        self.repoPath = repopath
+        self.remoteRepo = remoteRepo
+        self.makeRepo()
+        
+    def makeRepo(self):
         '''
         Simply checking for a .git directory in the current directory
         '''
                 
-        if not os.path.lexists( repopath + "/.git")  :
-            if remoterepo:
-                print "Cloning master repo: " + self.gitClone ( repopath, remoterepo )
+        if not os.path.lexists( self.repopath + "/.git")  :
+            if self.remoterepo:
+                print "Cloning master repo: " + self.gitClone ()
             else:
-                print "Creating local repo: " + self.gitInit ( repopath )
+                print "Creating local repo: " + self.gitInit ()
         else:
-            print "Git repo in " + repopath + " exists"
+            print "Git repo in " + self.repopath + " exists"
             
 
-    def gitInit (self, repodir):
+    def gitInit (self):
         '''
         Init the repo.
         
         This likely won't be used at all (Since clients should grab the latest version of the remote repo the first time they start, instead of creating their own).
         
         '''
-        if not os.path.lexists ( repodir ):
-            os.mkdir (repodir)
-        cmd = self.gitLocation + ' init'
-        pipe = subprocess.Popen(cmd, cwd=repodir)
-        pipe.wait()
+        if not self.checkRepoPath():
+            self.makeRepoPath()
+        cmd = 'init'
+        self.doGitCmd(cmd)
         
-        if not os.path.lexists ( repodir + "/.git"):
+        if not self.repoExists():
             # Even after we run the command and wait for it to complete, the .git directory in the target repository doesn't exist.
             print "Git repo doesn't exist after 'git init' invoked. Do you not have git installed, or is it not in your path?"
             os.sys.exit(1) #Fail
         
         #If we don't exit in the if above, return the created repo directory
-        return repodir
+        return self.repoPath
     
-    def gitClone (self,repodir,remoterepo):
+    def gitClone (self):
         '''
         Clone a remote repo.
         
         This should be the default behaviour. I'm not sure where the remote repo will be stored by default, but this should be configurable
         '''
         
-        if not os.path.lexists( repodir ):
-            os.mkdir ( repodir )
-        cmd = self.gitLocation + ' clone ' + remoterepo + " ." #Clone the repo into the remoterepo directory
-        pipe = subprocess.Popen(cmd, cwd=repodir)
-        pipe.wait()
+        if not self.checkRepoPath():
+            self.makeRepoPath()
+        cmd = 'clone ' + self.remoterepo + " ." #Clone the repo into the remoterepo directory
+        self.doGitCmd(cmd)
         
-        if not os.path.lexists ( repodir + "/.git"):
+        if not self.repoExists():
             # Even after we run the command and wait for it to complete, the .git directory in the target repository doesn't exist.
             print "Git repo doesn't exist after 'git clone' invoked. Do you not have git installed, or is it not in your path?"
             os.sys.exit(1) #Fail
 
-        return repodir
+        return self.repoPath
         
+    def gitStatus(self, porcelain=True):
+        pass
+    
+    
+    def doGitCmd(self, cmd):
+        '''
+        This method does the commands passed to it by other methods in the Repo Class.
+        It does the execution of git related commands. And returns the stdout from the command.
+        '''
+        cmd = Repo.gitLocation + ' ' + cmd.split(";")[0] #Split off any additional commands that may have gotten in.
+        pipe = subprocess.Popen()
+        pipe.wait()
         
+        return pipe.stdout
+    
+    def makeRepoPath(self):
+        try:
+            os.mkdir(self.repoPath)
+        except WindowsError:
+            pass # directory already exists.
+    
+    def checkRepoPath(self):
+        if os.path.lexists(self.repoPath):
+            return True
+        else:
+            return False
+    
+    def repoExists(self):
+        return os.path.lexists ( self.repoPath + "/.git")
+    
     def findGit(self):
         if platform.system() == "Windows":
             gits = ["git.exe","git.cmd"] #Windows version of git is referenced on the command line via these files
