cddea0c43f63a1bbd9e9dafcfebf66abcd906914
Lugghawk <acidictadpole@gmail.com>
2011-08-04 15:47:26 -0400

    Added a UI (still need some design to be done). No functionality in the UI yet.

--- src/notes/main.py
+++ src/notes/main.py
@@ -4,14 +4,29 @@ Created on 2011-07-15
 @author: Lugghawk and jenn0108
 '''
 
-if __name__ == '__main__':
-    pass
+import os, mypygit,sys, ui
 
-import os, mypygit
-
-homedir = os.path.expanduser("~")
+'''homedir = os.path.expanduser("~")
 repodir = os.path.normcase(homedir+"/sessionnotes/")
 
 remoteRepoDir = os.path.normcase(r"C:\SessionNotes\repo")
 
-repo = mypygit.Repo(repodir, remoteRepoDir)
+#repo = mypygit.Repo(repodir, remoteRepoDir)
+'''
+
+try:
+    import pygtk
+    pygtk.require("2.0")
+except:
+    pass
+
+try:
+    import gtk
+    import gtk.glade
+except:
+    sys.exit(1)
+  
+myUI = ui.UI()
+gtk.main()
+    
+    
